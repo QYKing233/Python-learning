98e4acf8f199646f2e4db7d21f52f59ed09bfb13
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2022-05-17 07:54:40 +0800

    Update mefrp.py

--- mefrp.py
+++ mefrp.py
@@ -1,6 +1,6 @@
 #  Python学习之路
 # 开发时间：2022/5/16 14:03
-# 用PYTHON来时实现内网穿透网站领流量
+# 用Python来实现内网穿透网站签到领流量
 
 import requests
 import re
@@ -13,7 +13,6 @@ def login(login_url, data):
 
 
 # 签到领流量
-# 开始分析签到的请求，由于我今天签到过了。签到按钮不能点击了，我们稍微改一下HTML
 def sign_in(csrf_url):
     # 这是签到的连接
     sign_in_url = f'https://mefrp.cn/?page=panel&module=sign&sign&csrf={get_csrf(csrf_url)}'
@@ -34,13 +33,13 @@ if __name__ == '__main__':
     login_url = 'https://mefrp.cn/?action=login&page=login'
     # 获取csrf的连接
     csrf_url = 'https://mefrp.cn/?page=panel&module=sign'
-    # post请求的登录用户名何密码
+    # post请求的登录用户名和密码
     datas = {
         'username': ['asdf123456'],
         'password': ['asdf123456']
     }
     data = {}
-    # 用session来维持会话
+    # 用session来维持登录信息
     session = requests.session()
 
     # 开始登录+签到
@@ -53,7 +52,5 @@ if __name__ == '__main__':
 # 这个CSRF是 每次请求都会改变的一个token  我们得先获取这个token
 # 我们用正则表达式提取出来
 # 然后我们把这个token与签到的url拼接起来
-# 这样看就是成功签到了 ，但是由于今天签到过了所以显示  您今天已经签到过了，请明天再来
-# 重新排版一下，理清一下逻辑
 # 如果多个账号怎么办，用字典+列表,然后用并行遍历
 # OJBK!
